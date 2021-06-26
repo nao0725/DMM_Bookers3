8bd3b5189df855d9d42a812173992a1d93e28f2e
nao0725 <aqua.725.times@gmail.com>
2021-06-26 08:17:31 +0000

    Create toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to:"homes#top"
+  get "homes/about" => "homes#about"
   resources:books,only:[:create, :index, :show, :edit, :update, :destroy]
   resources:users,only:[:index, :show, :edit, :update]
-  get "homes/top" => "homes#top"
-  get "homes/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
