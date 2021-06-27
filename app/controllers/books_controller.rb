f03e53a7aa738ab03bafe9184f620c48efc18a9e
nao0725 <aqua.725.times@gmail.com>
2021-06-27 05:28:27 +0000

    Create User info

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @book = Book.new
+    @books = Book.all 
+    @user = current_user
   end
 
   def show
+    @user = current_user
   end
   
   def edit
@@ -18,5 +26,10 @@ class BooksController < ApplicationController
   def destroy
   end
   
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
 
 
   def index
+    @users = User.all
+    @user = current_user
   end
 
   def show
+    @user = current_user
   end
 
   def edit
@@ -13,5 +16,9 @@ class UsersController < ApplicationController
   def update
   end
   
-  
+  private
+   
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
