6a64b17d4a3b1f154a17d1fada96400d8f53674d
nao0725 <aqua.725.times@gmail.com>
2021-06-28 09:52:24 +0000

    Create index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user = current_user
-    if @book.save
-     redirect_to book_path
+    if @book.
+      flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book)
     else
+      @user = current_user
+      @books = Book.all
       render :index
     end
   end
@@ -17,20 +20,30 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books = Book.all
+    @book = Book.all
+    @book_detail = Book.find(params[:id])
     @user = current_user
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path
+    else
+       
+    end
+    
   end
 
   def destroy
     @book = book.find(params[:id])
     @book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
   
   private
